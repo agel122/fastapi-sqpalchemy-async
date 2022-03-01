0afbf6a5939881e1f47dafb7ea75f8baa3e2c122
agel122 <agel122@yandex.ru>
2022-03-01 21:10:15 +0300

    [002]Added Routers

--- mynotes/routers.py
+++ mynotes/routers.py
@@ -21,23 +21,23 @@ async def get_records(author_id: int, session: AsyncSession = Depends(get_sessio
 
 
 @router.post('/add_record')
-async def create_record():
-    pass
+async def create_record(record: schemas.RecordCreate, session: AsyncSession = Depends(get_session)):
+    await create_record(record=record, db=session)
 
 
 @router.post('/add_author')
-async def create_author():
-    pass
+async def create_author(author: schemas.AuthorCreate, session: AsyncSession = Depends(get_session)):
+    await create_author(author=author, session=session)
 
 
 @router.delete('/record/{record_id}')
 async def delete_record(record_id: int, session: AsyncSession = Depends(get_session)):
-    return await crud.delete_record(db=session, record_id=record_id)
+    await crud.delete_record(db=session, record_id=record_id)
 
 
 @router.delete('/author/{author_id}')
 async def delete_author(author_id: int, session: AsyncSession = Depends(get_session)):
-    return await crud.delete_author(db=session, author_id=author_id)
+    await crud.delete_author(db=session, author_id=author_id)
 
 
 
