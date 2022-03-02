c69e989dc2bdb331f5eae3b5c402141d78235954
agel122 <agel122@yandex.ru>
2022-03-03 01:00:21 +0300

    [002]Added Routers

--- mynotes/database.py
+++ mynotes/database.py
@@ -4,10 +4,11 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 DATABASE_URL = "sqlite+aiosqlite:///./test.db"
 
 engine = create_async_engine(DATABASE_URL, connect_args={"check_same_thread": False}, echo=True)
-async_session = sessionmaker(autocommit=False, autoflush=False, bind=engine, class_=AsyncSession)
+async_session = sessionmaker(expire_on_commit=False, bind=engine, class_=AsyncSession)
 Base = declarative_base()
 
 
 async def get_session() -> AsyncSession:
     async with async_session() as session:
         yield session
+        await session.commit()
